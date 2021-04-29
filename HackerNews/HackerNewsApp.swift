0a1e75205853f1bb38339193a81d862d358b3f16
harshbsv <harshsolo.dev@gmail.com>
2021-04-29 22:25:43 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Harshvardhan Basava on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
